979182783d3b1e8db8c745e14c96c2bd32ceee98
Nexitron1 <nexitron044@gmail.com>
2026-05-02 09:40:26 +0600

    This is nexitron's branch

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("Hello World!")
-print("This is Nexitron's branch")
-input()
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("Error: there is no TELEGRAM_BOT_TOKEN in .env")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+print("Bot started to work")
+
+@bot.message_handler(commands=['start'])
+def send_welkome(message):
+    bot.reply_to(message, "Hi")
+
+if __name__ == "__main__":
+    bot.polling()
